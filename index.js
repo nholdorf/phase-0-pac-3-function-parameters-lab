8ce3737315105264a0264f1641e7cdd5743be4f9
Nicholas Holdorf <nicholas.holdorf@powerhrg.com>
2024-02-29 19:37:18 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function introduction(name) {
-    return 'Hi, my name is ${name}.';
+    return `Hi, my name is ${name}.`
 }
-introduction("Aki")
-introduction("Samip")
-console.log(introduction"Aki")
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
